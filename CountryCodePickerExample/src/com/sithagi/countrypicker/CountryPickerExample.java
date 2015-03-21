c3384bad8dd41a0b6b06ebea6754163596d957ce
Chathura Wijesinghe <cdanasiri@gmail.com>
2015-03-21 20:47:45 +0530

    add countrylist.json file

--- CountryCodePickerExample/src/com/sithagi/countrypicker/CountryPickerExample.java
+++ CountryCodePickerExample/src/com/sithagi/countrypicker/CountryPickerExample.java
@@ -15,58 +15,58 @@ import android.widget.Toast;
 public class CountryPickerExample extends FragmentActivity {
 
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_country_picker);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_country_picker);
 
-		FragmentManager manager = getSupportFragmentManager();
-		FragmentTransaction transaction = manager.beginTransaction();
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
 
-		CountryPicker picker = new CountryPicker();
-		picker.setListener(new CountryPickerListener() {
-			@Override
-			public void onSelectCountry(String name, String code, String dialCode) {
-				Toast.makeText(
-						CountryPickerExample.this,
-						"Country Name: " + name + " - Code: " + code
-								+ " - Currency: "
-								+ CountryPicker.getCurrencyCode(code) + " - Dial Code: "+dialCode,
-						Toast.LENGTH_SHORT).show();
-			}
-		});
+        CountryPicker picker = new CountryPicker();
+        picker.setListener(new CountryPickerListener() {
+            @Override
+            public void onSelectCountry(String name, String code, String dialCode) {
+                Toast.makeText(
+                        CountryPickerExample.this,
+                        "Country Name: " + name + " - Code: " + code
+                                + " - Currency: "
+                                + CountryPicker.getCurrencyCode(code) + " - Dial Code: " + dialCode,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 
-		transaction.replace(R.id.home, picker);
-		transaction.commit();
+        transaction.replace(R.id.home, picker);
+        transaction.commit();
 
-	}
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.country_picker, menu);
-		MenuItem item = menu.findItem(R.id.show_dialog);
-		item.setOnMenuItemClickListener(new OnMenuItemClickListener() {
-			
-			@Override
-			public boolean onMenuItemClick(MenuItem item) {
-				CountryPicker picker = CountryPicker.newInstance("Select Country");
-				picker.setListener(new CountryPickerListener() {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.country_picker, menu);
+        MenuItem item = menu.findItem(R.id.show_dialog);
+        item.setOnMenuItemClickListener(new OnMenuItemClickListener() {
 
-					@Override
-					public void onSelectCountry(String name, String code, String dialCode) {
-						Toast.makeText(
-								CountryPickerExample.this,
-								"Country Name: " + name + " - Code: " + code
-										+ " - Currency: "
-										+ CountryPicker.getCurrencyCode(code)+ " - Dial Code: "+dialCode,
-								Toast.LENGTH_SHORT).show();
-					}
-				});
-				
-				picker.show(getSupportFragmentManager(), "COUNTRY_CODE_PICKER");
-				return false;
-			}
-		});
-		return true;
-	}
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                CountryPicker picker = CountryPicker.newInstance("SelectCountry");
+                picker.setListener(new CountryPickerListener() {
+
+                    @Override
+                    public void onSelectCountry(String name, String code, String dialCode) {
+                        Toast.makeText(
+                                CountryPickerExample.this,
+                                "Country Name: " + name + " - Code: " + code
+                                        + " - Currency: "
+                                        + CountryPicker.getCurrencyCode(code) + " - Dial Code: " + dialCode,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                picker.show(getSupportFragmentManager(), "COUNTRY_CODE_PICKER");
+                return false;
+            }
+        });
+        return true;
+    }
 }
